274c82306690c4792bde1e0b270db9721652608a
JengYoung <young_pallete@naver.com>
2023-04-22 21:49:06 +0900

    chore: add svgr for svg components, with config

--- 3.x/next/10/next.config.js
+++ 3.x/next/10/next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    esmExternals: false,
+  future: {
+    webpack5: true
   },
+  webpack(config) {
+		config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    });
+    
+		return config;
+	},
 }
 
 module.exports = nextConfig
