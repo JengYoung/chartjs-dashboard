92cf3607dcc480767ad9cf202ad01faccbf20646
JengYoung <young_pallete@naver.com>
2023-04-23 13:29:44 +0900

    feat: addLogo with Text in Header

--- 3.x/next/10/components/Header.tsx
+++ 3.x/next/10/components/Header.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
-import Logo from './Icon/Logo'
+import styled, { useTheme } from 'styled-components'
+import { DefaultText } from './base/Text/DefaultText'
+import Logo from './base/Icon/Logo'
 
 export default function Header() {
+  const theme = useTheme();
+
   return (
     <Header.Container>
-      <Logo width="40px" height='40px'></Logo>
+      <Header.LogoContainer>
+        <Logo width="40px" height='40px' />
+        <DefaultText color={theme.color.white} fontWeight='700' margin="0 0 0 10px" fontSize="24px">DashBoard</DefaultText>
+      </Header.LogoContainer>
     </Header.Container>
   )
 }
 
 Header.Container = styled.div`
+  display: flex;
+  align-items: center;
   background-color: ${props => props.theme.color.primary['500']};
   height: 50px;
   padding: 0 20px;
   box-shadow: 0 0 4px 4px rgba(0,0,0,0.1);
+`
+
+
+Header.LogoContainer = styled.div`
+  display: flex;
 `
